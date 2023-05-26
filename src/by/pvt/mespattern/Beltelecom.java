102df5f8d4b87ab683ef2c0dd958df7d5eacfe94
artem <artem.yurev.1997@gmail.com>
2023-05-26 15:46:23 +0300

    task

--- src/by/pvt/mespattern/Beltelecom.java
+++ src/by/pvt/mespattern/Beltelecom.java
@@ -1,18 +1,5 @@
 package by.pvt.mespattern;
 
-public class Beltelecom extends Operator implements Observer{
-    public Beltelecom(String message) {
-        super(message);
-    }
-    private static String attention = "Звонить всем абонентам и уведомлять об опасности купания: ";
+public class Beltelecom {
 
-    @Override
-    public void generate(String message) {
-        System.out.println(attention + message);
-    }
-
-    @Override
-    public void send(String message) {
-        System.out.println("Сообщение отправлено от Beltelecom! " + message);
-    }
 }
